ea1b542688257228ad806a68f7d6db57c710f2aa
LC-burigo <60100864+LC-burigo@users.noreply.github.com>
2020-10-12 17:29:55 -0300

    Update third_project.py

--- third_project.py
+++ third_project.py
@@ -1,25 +1,52 @@
-from random import shuffle
-
-SUITE = 'H D S C'.split()
-RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+print("Estes resultados abaixo são relativos à funções apenas")
 
 
-class Deck:
-
-    def __init__(self):
-        print("Creating New Ordered Deck")
-        self.allcards = [(s, r) for s in SUITE for r in RANKS]
-
-    def shuffle(self):
-        print("Shuffling Deck")
-        shuffle(self.allcards)
-
-    def split_in_half(self):
-        return self.allcards[:26], self.allcards[26:]
+def Fator_Parcelamento(taxa_de_juros, numero_de_parcelas):
+    FP = (((1 + taxa_de_juros)**numero_de_parcelas) - 1)/(((1 + taxa_de_juros)**numero_de_parcelas) * taxa_de_juros)
+    return FP
 
 
-deck_one = Deck()
-deck_one.shuffle()
-first_half, second_half = deck_one.split_in_half()
-print(first_half)
-print(second_half)
+def Total_a_pagar(valor_a_vista, x, y, func):
+    Valor_parcela = valor_a_vista/func(x, y)
+    total_pagar = Valor_parcela * y
+    print(Valor_parcela)
+    return total_pagar
+
+
+print(Fator_Parcelamento(2/100, 30))
+teste = Total_a_pagar(2, 2/100, 30, Fator_Parcelamento)
+print(teste)
+print("#" * 30)
+
+print("Estes resultados abaixo são relativos à uma classe")
+
+
+class Pagamento:
+    def __init__(self, taxa_de_juros, numero_de_parcelas, valor_a_vista):
+        self.taxa_de_juros = taxa_de_juros
+        self.numero_de_parcelas = numero_de_parcelas
+        self.valor_a_vista = valor_a_vista
+
+    def fator_de_parcelamento(self):
+        fp = (((1 + self.taxa_de_juros) ** self.numero_de_parcelas) - 1) / (
+                    ((1 + self.taxa_de_juros) ** self.numero_de_parcelas) * self.taxa_de_juros)
+        return fp
+
+    def valor_parcela(self):
+        vp = self.valor_a_vista/self.fator_de_parcelamento()
+        return vp
+
+    def valor_total(self):
+        vt = self.valor_parcela() * self.numero_de_parcelas
+        return vt
+
+
+Lucas = Pagamento(2/100, 30, 2)
+print(Lucas.fator_de_parcelamento())
+print(Lucas.valor_parcela())
+print(Lucas.valor_total())
+
+
+
+
+
